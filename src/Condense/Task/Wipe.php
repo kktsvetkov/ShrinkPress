55ff032aa86a88e26d1395c7e31ab638f7d1bc53
Kaloyan K. Tsvetkov <kaloyan@kaloyan.info>
2020-03-24 23:25:17 +0200

    src/Condense/Task/Wipe.php

---
+++ src/Condense/Task/Wipe.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ShrinkPress\Build\Condense\Task;
+
+use ShrinkPress\Build\Project;
+use ShrinkPress\Build\Condense;
+
+class Wipe extends TaskAbstract
+{
+	const purge = array(
+		'composer.json',
+		'.gitignore',
+		'.gitattributes',
+		Condense\Compat::compatibility_php,
+		);
+
+	function condense(
+		Project\Source $source,
+		Project\Storage\StorageAbstract $storage
+		)
+	{
+		foreach (self::purge as $file)
+		{
+			if ($source->exists($file))
+			{
+				$source->unlink($file);
+			}
+		}
+
+		chdir( $source->basedir() );
+		shell_exec('git checkout -- .');
+	}
+}
