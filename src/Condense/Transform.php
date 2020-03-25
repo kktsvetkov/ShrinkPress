4aec0d191463e4c6fead794dc033991e9e7aafba
Kaloyan K. Tsvetkov <kaloyan@kaloyan.info>
2020-03-25 10:51:35 +0200

    src/Condense/Transform.php

--- src/Condense/Transform.php
+++ src/Condense/Transform.php
@@ -8,4 +8,14 @@ class Transform
 	{
 		return $name;
 	}
+
+	static function wpNamespace($package)
+	{
+		$namespace = 'ShrinkPress\\' . trim($package, '\\') . '\\';
+		$folder = Composer::vendors . '/shrinkpress/'
+			. str_replace('\\', '/', strtolower($package))
+			. '/src';
+
+		return array($namespace, $folder);
+	}
 }
