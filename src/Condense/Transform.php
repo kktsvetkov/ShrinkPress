b5c50387eaad8e6d9503fdbdc2dc1289c4c77e9b
Kaloyan K. Tsvetkov <kaloyan@kaloyan.info>
2020-03-27 08:31:14 +0200

    src/Condense/Transform.php

--- src/Condense/Transform.php
+++ src/Condense/Transform.php
@@ -4,8 +4,24 @@ namespace ShrinkPress\Build\Condense;
 
 class Transform
 {
+	const function_prefix = array(
+		'wp_ajax_',
+		'wp_',
+	);
+
 	static function wpFunction($name)
 	{
+		$prefix = false;
+		foreach (self::function_prefix as $p)
+		{
+			if (0 === strpos($name, $p))
+			{
+				$name = substr($name, strlen($p));
+				$prefix = true;
+				break;
+			}
+		}
+
 		return $name;
 	}
 
