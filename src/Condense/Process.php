974b50435b11ba58121d9c28d7965d7035dea60b
Kaloyan K. Tsvetkov <kaloyan@kaloyan.info>
2020-03-31 11:04:41 +0300

    src/Condense/Process.php

--- src/Condense/Process.php
+++ src/Condense/Process.php
@@ -21,9 +21,11 @@ class Process
 		$this->tasks[] = new Task\Wipe;
 		$this->tasks[] = new Task\Start;
 
-		$this->tasks[] = new Task\FunctionsMap;
+		// $this->tasks[] = new Task\FunctionsMap;
 		$this->tasks[] = new Task\SortFunctions;
 		$this->tasks[] = new Task\ReplaceFunctions;
+
+		$this->tasks[] = new Task\UseNamespaces;
 	}
 
 	function condense()
