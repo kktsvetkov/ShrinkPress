6ddbb57ee144094e855f30d56bb920fafe66ecfb
Kaloyan K. Tsvetkov <kaloyan@kaloyan.info>
2020-04-06 22:12:38 +0300

    src/Assist/ComposerPhar.php

---
+++ src/Assist/ComposerPhar.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ShrinkPress\Build\Assis;
+
+use ShrinkPress\Build\Verbose;
+
+class ComposerPhar
+{
+	const url = 'https://getcomposer.org/composer.phar';
+
+	static function get()
+	{
+		$local = __DIR__ . '/../../composer.phar';
+		if (!file_exists($local))
+		{
+			Verbose::log('Downloading composer.phar', 2);
+			shell_exec(
+				'curl -s ' . self::url
+					. ' -O ' . escapeshellcmd($local)
+				);
+
+			if (!file_exists($local))
+			{
+				throw new \RuntimeException(
+					'Unable to download composer.phar'
+				);
+			}
+		}
+
+		return realpath($local);
+	}
+
+	function dumpautoload($basedir)
+	{
+		$basedir = (string) $basedir;
+		if (!is_dir($basedir))
+		{
+			throw new \InvalidArgumentException(
+				"Provided \$basedir must be an existing folder."
+			);
+		}
+		chdir($basedir);
+
+		$composer_phar = self::get();
+		shell_exec(
+			'php ' . escapeshellcmd($composer_phar) . ' dumpautoload'
+			);
+	}
+}
