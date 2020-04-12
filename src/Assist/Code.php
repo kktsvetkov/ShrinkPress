7452e02dbcb65e477536691039e9639374ad580f
Kaloyan K. Tsvetkov <kaloyan@kaloyan.info>
2020-04-12 08:16:43 +0300

    src/Assist/Code.php

--- src/Assist/Code.php
+++ src/Assist/Code.php
@@ -153,4 +153,21 @@ class Code
 		$code = "\t" . rtrim($code, "\t");
 		return $code;
 	}
+
+	static function extractPackage($code, $entity)
+	{
+		$doccomment = substr($code, 0, 1024);
+
+		if (preg_match('~\s*\*\s+@package\s+(.+)\s+\*~Uis', $doccomment, $R))
+		{
+			$entity->docPackage = $R[1];
+		}
+
+		if (preg_match('~\s*\*\s+@subpackage\s+(.+)\s+\*~Uis', $doccomment, $R))
+		{
+			$entity->docSubPackage = $R[1];
+		}
+
+		return $entity;
+	}
 }
