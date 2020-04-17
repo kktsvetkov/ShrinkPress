460c9094e642cb7210d58cb3e270cba9520e1a2d
Kaloyan K. Tsvetkov <kaloyan@kaloyan.info>
2020-04-17 08:47:38 +0300

    src/Entity/

--- src/Entity/Funcs/WordPress_Func.php
+++ src/Entity/Funcs/WordPress_Func.php
@@ -6,5 +6,24 @@ use ShrinkPress\Build\Entity;
 
 class WordPress_Func extends Function_Abstract
 {
+	protected $callbacks = array();
 
+	function addCallback($filename, $line, $hook, $caller)
+	{
+		$callback = array(
+			(string) $filename,
+			(int) $line,
+			(string) $hook,
+			(string) $caller
+			);
+
+		$this->callbacks[ "{$callback[0]}:{$callback[1]}" ] = $callback;
+
+		return $this;
+	}
+
+	function getCallbacks()
+	{
+		return $this->callbacks;
+	}
 }
