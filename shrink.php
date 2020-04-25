d39ba01d5df1c8cff1e12463c9c9f01390f03d06
Kaloyan K. Tsvetkov <kaloyan@kaloyan.info>
2020-04-25 15:23:36 +0300

    shrink.php

--- shrink.php
+++ shrink.php
@@ -16,27 +16,9 @@ use ShrinkPress\Reframe;
 $wp_source = __DIR__ . '/wordpress';
 Reframe\Assist\Verbose::level(4);
 
-$index1 = new Reframe\Index\Index_PDO(
-	new PDO("mysql:host=127.0.0.1;dbname=wordpress;charset=utf8mb4",
-		'username',
-		'password',
-		array(
-		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-		PDO::ATTR_EMULATE_PREPARES => false,
-	)));
-
-$index2 = new Reframe\Index\Index_Stash(
+$index = new Reframe\Index\Index_Stash(
 	new Reframe\Assist\Umbrella(__DIR__ . '/entities')
-);
-
-$index3 = new Reframe\Index\Index_Dummy;
-
-$index4 = new Reframe\Index\Index_Nested;
-$index4->addNested($index1);
-$index4->addNested($index2);
-$index4->addNested($index3);
-
-$index = $index2;
+	);
 
 if (in_array('scan', $argv))
 {
@@ -47,7 +29,6 @@ if (in_array('scan', $argv))
 
 	$source = new Reframe\Parse\Source(__DIR__ . '/wordpress');
 	$scanner = new Reframe\Parse\Scanner($source, $index);
-	// $scanner->scanFolder('wp-includes/');
 	$scanner->scanFolder('');
 
 	echo "Files: ", count( $index->getFiles() ), " found\n";
@@ -61,6 +42,11 @@ if (in_array('scan', $argv))
 if (in_array('build', $argv))
 {
 	$source = new Reframe\Unparse\Source(__DIR__ . '/reduced');
+
+	$modified = new Reframe\Index\Index_Stash(
+		new Reframe\Assist\Umbrella(__DIR__ . '/modified')
+		);
+
 	$process = new Reframe\Unparse\Builder;
 	$process->build($source, $index);
 }
