964978a4c4fec6a8056c68dbd6f3d56468c70af1
Kaloyan K. Tsvetkov <kaloyan@kaloyan.info>
2020-04-28 11:50:13 +0300

    remove error handling, rely on "brutalano"

--- evolve.php
+++ evolve.php
@@ -3,14 +3,6 @@ include __DIR__ . '/vendor/autoload.php';
 
 ini_set('xdebug.max_nesting_level', 3000);
 
-error_reporting(E_ALL);
-set_error_handler(function($severity, $message, $file, $line)
-{
-	throw new ErrorException($message, 0, $severity, $file, $line);
-}, E_ALL);
-
-/////////////////////////////////////////////////////////////////////////////
-
 use ShrinkPress\Reframe;
 
 new Reframe\Evolve\Scan(__DIR__ . '/reduced');
